a4e9a5895c62286611fdb8cd65f9abee2ef11522
Otto Castle <otto.castle1@gmail.com>
2020-01-21 18:34:57 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="otto",
+    version="0.0.1",
+    author="Otto Castle",
+    author_email="otto.castle1@gmail.com",
+    description="Python tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+
