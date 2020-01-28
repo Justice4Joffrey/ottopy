dbb3496b57d9e91c8323bfda3de0d4ed246bedc4
otto <otto.castle1@gmail.com>
2020-01-28 18:08:38 +0000

    delete

--- ottopy/dt/formats.py
+++
@@ -1,9 +0,0 @@
-from typing import NamedTuple
-
-
-class _DtFormatStrType(NamedTuple):
-    LOGGING_DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S.%f%z"
-    FILENAME_FORMAT: str = "%Y-%m-%d.%H-%M-%S.%f%z"
-
-
-DtFormatStr = _DtFormatStrType()
