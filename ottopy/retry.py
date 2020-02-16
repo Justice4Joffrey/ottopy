86d9e0963fc07e092fc95cfaf8e2a23160b0fc83
Otto Castle <otto.castle1@gmail.com>
2020-02-16 11:49:55 +0000

    call retry

--- ottopy/retry.py
+++ ottopy/retry.py
@@ -1,6 +1,6 @@
 import time
 from dataclasses import dataclass, field
-from typing import Any, Callable, Optional, Type, Union, Tuple
+from typing import Any, Callable, Optional, Type, Union, Tuple, TypeVar, Dict
 
 ExceptionType = Union[Type[Exception], Tuple[Type[Exception], ...]]
 
@@ -14,9 +14,23 @@ class CallResponse:
     exception: Optional[Exception] = field(default=None)
 
 
+T = TypeVar("T")
+
+
+def _fn_string(
+    fn: Callable[..., T],
+    validator: Callable[[T], T],
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+) -> str:
+    all_fn_args = tuple(map(str, args)) + tuple(f"{k}={v}" for k, v in kwargs.items())
+    all_args_str = ", ".join(all_fn_args)
+    return f"{validator.__name__}({fn.__name__}({all_args_str}))"
+
+
 def call_retry(
-    fn: Callable,
-    validator: Callable,
+    fn: Callable[..., T],
+    validator: Callable[[T], T],
     exceptions: ExceptionType,
     sleep: int,
     retries: int,
@@ -31,10 +45,9 @@ def call_retry(
             exc = e
             time.sleep(sleep)
     else:
-        all_fn_args = args + tuple(f"{k}={v}" for k, v in kwargs.items())
-        all_args_str = ", ".join(all_fn_args)
-        fn_str = f"{fn.__name__}({all_args_str})"
         exception = RuntimeError(
-            f"Failed {retries} times calling {fn_str}. Last exception: {exc}"
+            f"Failed {retries} times "
+            f"calling {_fn_string(fn, validator, args, kwargs)}. "
+            f"Last exception: {exc}"
         )
         return CallResponse(None, retries, False, exc, exception=exception)
