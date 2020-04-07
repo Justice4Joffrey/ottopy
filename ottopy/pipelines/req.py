5c506c971d05830d6313d0ce51e5a50346d84c82
Otto Castle <otto.castle1@gmail.com>
2020-04-07 15:50:54 +0100

    meg

--- ottopy/pipelines/req.py
+++ ottopy/pipelines/req.py
@@ -1,5 +1,5 @@
 from requests import Session, Request, Response, PreparedRequest
-from returns.io import impure_safe, IOResultE, IOResult
+from returns.io import impure_safe, IOResultE
 from returns.pipeline import flow
 from returns.pointfree import bind
 from returns.result import safe, ResultE
@@ -7,33 +7,31 @@ from returns.result import safe, ResultE
 
 def send_request(
     session: Session, request: Request, *, allow_redirects: bool = True
-) -> ResultE[Response]:
-    def _prepare(req: Request) -> ResultE[PreparedRequest]:
+) -> IOResultE[Response]:
+    def _lprepare(req: Request) -> ResultE[PreparedRequest]:
         return prepare(session, req)
 
-    def _execute(prepared_request: PreparedRequest) -> IOResultE[Response]:
+    def _lexecute(prepared_request: PreparedRequest) -> IOResultE[Response]:
         return execute(session, prepared_request, allow_redirects=allow_redirects)
 
-    return flow(request, _prepare, bind(_execute), IOResult.lift_result)
+    return flow(request, _lprepare, bind(_lexecute))
 
 
 def send_json_request(
     session: Session, request: Request, *, allow_redirects: bool = True
 ) -> ResultE[Response]:
-    def _send_request(req: Request) -> ResultE[Response]:
+    def _lsend_request(req: Request) -> IOResultE[Response]:
         return send_request(session, req, allow_redirects=allow_redirects)
 
-    return flow(request, _send_request, bind(validate_json), IOResult.lift_result)
+    return flow(request, _lsend_request, bind(validate_json))
 
 
-@safe
-def validate_json(response: Response) -> Response:
+def _validate_json(response: Response) -> Response:
     response.json()
     return response
 
 
-@impure_safe
-def execute(
+def _execute(
     session: Session, prepared_request: PreparedRequest, *, allow_redirects: bool = True
 ) -> Response:
     resp = session.send(prepared_request, allow_redirects=allow_redirects)
@@ -41,6 +39,15 @@ def execute(
     return resp
 
 
-@safe
-def prepare(session: Session, request: Request) -> PreparedRequest:
+def _prepare(session: Session, request: Request) -> PreparedRequest:
     return session.prepare_request(request)
+
+
+# the typing of decorators doesn't seem to work so great
+validate_json = safe(_validate_json)
+prepare = safe(_prepare)
+execute = impure_safe(_execute)
+
+
+res = send_json_request(Session(), Request("GET", "http://www.google.com"))
+print(123)
