efd43d80a65a3065d63219b7cd959ff2c59f3146
Otto Castle <otto.castle1@gmail.com>
2020-04-09 15:18:01 +0100

    stop this functional wankfest

--- ottopy/pipelines/req.py
+++ ottopy/pipelines/req.py
@@ -1,37 +1,28 @@
 from requests import Session, Request, Response, PreparedRequest
-from returns.io import impure_safe, IOResultE
-from returns.pipeline import flow
-from returns.pointfree import bind
-from returns.result import safe, ResultE
 
 
 def send_request(
     session: Session, request: Request, *, allow_redirects: bool = True
-) -> IOResultE[Response]:
-    def _lprepare(req: Request) -> ResultE[PreparedRequest]:
-        return prepare(session, req)
-
-    def _lexecute(prepared_request: PreparedRequest) -> IOResultE[Response]:
-        return execute(session, prepared_request, allow_redirects=allow_redirects)
-
-    return flow(request, _lprepare, bind(_lexecute))
+) -> Response:
+    prep = prepare(session, request)
+    resp = execute(session, prep, allow_redirects=allow_redirects)
+    return resp
 
 
 def send_json_request(
     session: Session, request: Request, *, allow_redirects: bool = True
-) -> ResultE[Response]:
-    def _lsend_request(req: Request) -> IOResultE[Response]:
-        return send_request(session, req, allow_redirects=allow_redirects)
+) -> Response:
+    resp = send_request(session, request, allow_redirects=allow_redirects)
+    validate_json(resp)
+    return resp
 
-    return flow(request, _lsend_request, bind(validate_json))
 
-
-def _validate_json(response: Response) -> Response:
+def validate_json(response: Response) -> Response:
     response.json()
     return response
 
 
-def _execute(
+def execute(
     session: Session, prepared_request: PreparedRequest, *, allow_redirects: bool = True
 ) -> Response:
     resp = session.send(prepared_request, allow_redirects=allow_redirects)
@@ -39,15 +30,5 @@ def _execute(
     return resp
 
 
-def _prepare(session: Session, request: Request) -> PreparedRequest:
+def prepare(session: Session, request: Request) -> PreparedRequest:
     return session.prepare_request(request)
-
-
-# the typing of decorators doesn't seem to work so great
-validate_json = safe(_validate_json)
-prepare = safe(_prepare)
-execute = impure_safe(_execute)
-
-
-res = send_json_request(Session(), Request("GET", "http://www.google.com"))
-print(123)
