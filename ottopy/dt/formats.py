dc4cd721ffbc511140913d844cf16a7a7e6471d0
Otto Castle <otto.castle1@gmail.com>
2020-04-13 18:40:37 +0100

    add a zulu time parseing fn

--- ottopy/dt/formats.py
+++ ottopy/dt/formats.py
@@ -4,6 +4,14 @@ from typing import NamedTuple
 class _DtFormatStrType(NamedTuple):
     LOGGING_DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S.%f%z"
     FILENAME_FORMAT: str = "%Y-%m-%d.%H-%M-%S.%f%z"
+    FILENAME_FORMAT_SEC: str = "%Y-%m-%d.%H-%M-%S%z"
+
+
+def parse_zulu_format(string: str) -> str:
+    char = "Z"
+    if not string.endswith(char):
+        raise ValueError(f"String {string} must end with {char}")
+    return string.replace(char, "+0000")
 
 
 DtFormatStr = _DtFormatStrType()
