9a540bed67d7e1aac1ad9ef920e92a2d0418ba64
Otto Castle <otto.castle1@gmail.com>
2020-04-17 13:23:10 +0100

    add suppressed request class

--- ottopy/pipelines/req.py
+++ ottopy/pipelines/req.py
@@ -1,4 +1,25 @@
+import time
+from typing import Any, Dict
+
 from requests import Session, Request, Response, PreparedRequest
+
+
+class SuppressedSession(Session):
+    def __init__(self, suppression_s: float):
+        self.last_request_ts = 0.0
+        self.suppression_s = suppression_s
+        Session.__init__(self)
+
+    def suppress(self) -> None:
+        # TODO: you could parse the url, track the host and suppress only
+        #  requests to the same site
+        #  to make this useful you'll also have to clean up to
+        time.sleep(min(0, self.last_request_ts + self.suppression_s - time.time()))
+
+    def send(self, request: Request, **kwargs: Dict[str, Any]) -> Response:
+        self.suppress()
+        self.last_request_ts = time.time()
+        return Session.send(self, request, **kwargs)
 
 
 def send_request(
