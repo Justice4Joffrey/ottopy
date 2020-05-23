8bb7eaaaa6ba954580c1bdae11b256c328706a94
Otto Castle <otto.castle1@gmail.com>
2020-05-23 11:37:33 +0100

    structlog add wrapper to argument calls

--- ottopy/log/structured/__init__.py
+++ ottopy/log/structured/__init__.py
@@ -1,9 +1,14 @@
 import json
 import os
-from typing import Tuple, Dict, Any, Optional, Union
+from typing import Tuple, Dict, Any, Optional, Union, Callable, Type
 
-from structlog import get_logger, configure, processors
-from structlog._config import BoundLoggerLazyProxy as StructLogger
+from structlog import (
+    get_logger,
+    configure,
+    processors as _processors,
+    stdlib,
+    BoundLogger,
+)
 
 from ottopy.dt import DateTime, strftime, utcnow
 from ottopy.dt.formats import DtFormatStr
@@ -12,9 +17,12 @@ from ottopy.log import Logger
 __all__ = [
     "get_struct_logger",
     "parse_log_line",
+    "stdlib",
     "ParsedLogLine",
+    "Processor",
     "StructLogger",
     "EventDict",
+    "DEFAULT_PROCESSORS",
     "EVENT",
     "LOG_TS_KEY",
 ]
@@ -23,9 +31,12 @@ LOG_TS_KEY = "_log_ts"
 EVENT = "event"
 
 EventDict = Dict[str, Any]
+Processor = Callable[[Logger, str, EventDict], EventDict]
 ParsedLogLine = Tuple[DateTime, str, EventDict]
 ParsedLogLineResult = Tuple[Optional[ParsedLogLine], Optional[str]]
-StructLogger = StructLogger
+
+StructLogger = Type[BoundLogger]
+stdlib = stdlib
 
 
 def timestamper(_: Logger, __: str, event_dict: EventDict) -> EventDict:
@@ -34,13 +45,20 @@ def timestamper(_: Logger, __: str, event_dict: EventDict) -> EventDict:
     return event_dict
 
 
-def get_struct_logger() -> StructLogger:
+DEFAULT_PROCESSORS: Tuple[Processor, ...] = (
+    timestamper,
+    _processors.JSONRenderer(sort_keys=True, serializer=json.dumps),
+)
+
+
+def get_struct_logger(
+    *,
+    processors: Tuple[Processor, ...] = DEFAULT_PROCESSORS,
+    wrapper_class: Optional[StructLogger] = None,
+) -> StructLogger:
     """Create a struct logger which creates each log line as a timestamped JSON"""
     configure(
-        processors=[
-            timestamper,
-            processors.JSONRenderer(sort_keys=True, serializer=json.dumps),
-        ],
+        processors=list(processors), wrapper_class=wrapper_class,
     )
     return get_logger()
 
