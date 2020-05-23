69c922ca3afe97048bc90a569289cc7cb29acf42
Otto Castle <otto.castle1@gmail.com>
2020-05-23 12:13:57 +0100

    add a plain formatter

--- ottopy/log/consts.py
+++ ottopy/log/consts.py
@@ -1,6 +1,9 @@
+from ottopy.log import Formatter
 from ottopy.log.classes import UTCMicroSecFormatter
 
 DEFAULT_SUFFIX = ".log"
 
 FORMATTER = UTCMicroSecFormatter("{asctime} {name} {levelname} >> {message}", style="{")
 RAW_FORMATTER = UTCMicroSecFormatter("{asctime} >> {message}", style="{")
+# Useful if you're using structlog
+PLAIN_FORMATTER = Formatter("{message}", style="{")
