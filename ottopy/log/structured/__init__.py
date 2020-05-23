9280fae8a960121355cb477b3f7895eb3bdb09f8
Otto Castle <otto.castle1@gmail.com>
2020-05-23 10:57:00 +0100

    structlog helper

--- ottopy/log/structured/__init__.py
+++ ottopy/log/structured/__init__.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import Tuple, Dict, Any, Optional, Union
 
 from structlog import get_logger, configure, processors
@@ -24,6 +25,7 @@ EVENT = "event"
 EventDict = Dict[str, Any]
 ParsedLogLine = Tuple[DateTime, str, EventDict]
 ParsedLogLineResult = Tuple[Optional[ParsedLogLine], Optional[str]]
+StructLogger = StructLogger
 
 
 def timestamper(_: Logger, __: str, event_dict: EventDict) -> EventDict:
@@ -50,3 +52,7 @@ def parse_log_line(line: Union[str, bytes]) -> ParsedLogLineResult:
     except json.JSONDecodeError as e:
         return None, e.msg
     return (data.pop(LOG_TS_KEY), data.pop(EVENT), data), None
+
+
+def log_startup_message(logger: StructLogger, name: str, git_rev: str) -> None:
+    logger.msg(f"Started process", name=name, git_rev=git_rev, pid=os.getpid())
